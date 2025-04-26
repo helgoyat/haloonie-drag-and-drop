e66a461161b09c5b38d15a40f13b1eea37442949
Helene Goyat <goyat.helene@gmail.com>
2025-04-26 10:33:58 -0700

    Use tailwindcss and re-style

--- src/components/DraggableBox.vue
+++ src/components/DraggableBox.vue
@@ -20,12 +20,16 @@ function onDrag(event: MouseEvent) {
   const sandboxElement = document.getElementById("sandbox");
   if (!sandboxElement || !draggableElementRef.value) return;
   const sandboxElementRect = sandboxElement.getBoundingClientRect();
-  const draggableElementRect = draggableElementRef.value.getBoundingClientRect();
+  const draggableElementRect =
+    draggableElementRef.value.getBoundingClientRect();
 
   const left = event.clientX - sandboxElementRect.left - pointerX.value;
   const top = event.clientY - sandboxElementRect.top - pointerY.value;
 
-  if (left >= 0 && left <= sandboxElementRect.width - draggableElementRect.width) {
+  if (
+    left >= 0 &&
+    left <= sandboxElementRect.width - draggableElementRect.width
+  ) {
     draggableElementRef.value.style.left = left + "px";
   } else if (left < 0) {
     draggableElementRef.value.style.left = 0 + "px";
@@ -34,7 +38,10 @@ function onDrag(event: MouseEvent) {
       sandboxElementRect.width - draggableElementRect.width + "px";
   }
 
-  if (top >= 0 && top <= sandboxElementRect.height - draggableElementRect.height) {
+  if (
+    top >= 0 &&
+    top <= sandboxElementRect.height - draggableElementRect.height
+  ) {
     draggableElementRef.value.style.top = top + "px";
   } else if (top < 0) {
     draggableElementRef.value.style.top = 0 + "px";
@@ -51,25 +58,11 @@ function onDragEnd(event: MouseEvent) {
 </script>
 
 <template>
-  <div ref="draggableElementRef" class="draggable" @pointerdown="onDragStart">
+  <div
+    ref="draggableElementRef"
+    class="absolute top-0 left-0 bg-lime-500 text-lime-100 cursor-grab inline select-none py-3 px-4 rounded active:cursor-grabbing"
+    @pointerdown="onDragStart"
+  >
     <slot>Drag Me</slot>
   </div>
 </template>
-
-<style scoped>
-.draggable {
-  position: absolute;
-  top: 10px;
-  left: 10px;
-  background-color: orangered;
-  color: white;
-  cursor: grab;
-  display: inline;
-  user-select: none;
-  padding: 10px;
-  border-radius: 4px;
-}
-.draggable:active {
-  cursor: grabbing;
-}
-</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,15 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>
-    <h1>Drag and Drop</h1>
-    <div id="sandbox">
-      <draggable-box></draggable-box>
+  <div class="flex flex-col items-center gap-8">
+    <h1 class="text-3xl font-bold text-white">Haloonie - Drag and Drop</h1>
+    <div
+      id="sandbox"
+      class="relative bg-orange-500 w-[400px] aspect-square rounded"
+    >
       <draggable-box></draggable-box>
     </div>
   </div>
 </template>
 
-<style scoped>
-#sandbox {
-  position: relative;
-  background-color: darkslateblue;
-  height: 300px;
-  border-radius: 4px;
-}
-</style>
+<style scoped></style>
